703191f8a66b724800297ed27582d9717a8bc296
sumanth <23951a12d7@iare.a.in>
2026-02-16 22:15:55 +0530

    FORCE: Removing all UI component imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,15 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import Home from "@/pages/Home";
-import Projects from "@/pages/Projects";
-import NotFound from "@/pages/not-found";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import NotFound from "./pages/not-found";
 
-function Router() {
+function App() {
   return (
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/projects" component={Projects} />
       <Route component={NotFound} />
     </Switch>
-  );
-}
-
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Router />
-    </QueryClientProvider>
   );
 }
 
